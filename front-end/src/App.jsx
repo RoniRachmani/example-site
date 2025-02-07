51533856cb9cba536790143599dd1836b769465a
Roni Rachmani <RoniRachmani.hey@ronirachmani.com>
2025-02-07 18:55:58 +0200

    initialize front-end project with React and Vite setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,40 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import Layout from './Layout';
+
+const routes = [{
+  path: '/',
+  element: <Layout />,
+  children: [{
+    path: '/',
+    element: <HomePage />
+  }, {
+    path: '/about',
+    element: <AboutPage />
+  }, {
+    path: '/articles',
+    element: <ArticlesListPage />
+  }, {
+    path: '/articles/:name', // -> /articles/learn-react
+    element: <ArticlePage />
+  }]
+}]
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return (
+    <>
+    <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App
